aeddc6a9d427dc992814542d8afe33af4dc799f4
garywkh-git <garywkh@gmail.com>
2026-01-05 12:17:57 +0800

    Step 12 : logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('register', views.register, name='register'),
-    path('login', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -165,6 +165,6 @@ INTERNAL_IPS = ["127.0.0.1"]
 
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
-    messages.SUCCESS: 'sucess',
+    messages.SUCCESS: 'success',
 
 }
